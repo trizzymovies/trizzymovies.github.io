4c9e19a873ac4e40d88225382e9b35166426139c
Trizzyrwa <ztcgang05@gmail.com>
2026-09-16 12:24:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const movies = [
     genre: "Action • Episode 12",
     description: "Young Swordsman Episode 12.",
     poster:
-      poster: "images/young-swordsman.jpg",
+      poster: "Screenshot_20260916-075509.png",
       "https://pub-945fd499c47346f1923953fd79ef28c5.r2.dev/YOUNG%20SWORDSMAN%20EP12.mp4"
   },
 
