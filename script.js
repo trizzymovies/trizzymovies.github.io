c3369b4d1536595904f67fa809e5e205bf0c062b
Trizzyrwa <ztcgang05@gmail.com>
2026-09-16 12:45:08 +0200

    Fix formatting and add newline at end of script.js

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ const movies = [
     genre: "Action • Episode 12",
     description: "Young Swordsman Episode 12.",
     poster: "Screenshot_20260916-075509.png",
-video: "https://pub-945fd499c47346f1923953fd79ef28c5.r2.dev/YOUNG%20SWORDSMAN%20EP12.mp4"
+    video: "https://pub-945fd499c47346f1923953fd79ef28c5.r2.dev/YOUNG%20SWORDSMAN%20EP12.mp4"
+  }
+];
